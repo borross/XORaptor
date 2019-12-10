669096955d0848a1356726491a1a977579c27e44
Boris <39199196+borross@users.noreply.github.com>
2019-12-10 12:49:03 +0300

    Update XORfile3.py

--- XORfile3.py
+++ XORfile3.py
@@ -6,6 +6,14 @@ def  xor(inputDATA, key, outputFILE):
     keyLEN = len(key)
     return open(outputFILE, 'wb').write(bytearray(((inputDATA[i] ^ key[i % keyLEN]) for i in range(0, len(inputDATA)))))
 
+def fastXor(inputDATA, key, outputFILE):
+    from math import ceil
+    DataLen = len(inputDATA)
+
+    #key with same length as file
+    kkey = (key*int(ceil(float(DataLen)/float(len(key)))))[:DataLen]
+    return open(outputFILE, 'wb').write(bytearray(((inputDATA[i] ^ kkey[i]) for i in range(0, DataLen))))
+
 def secure_delete_rand(file, passes=1):
 	seed()
 	with open(file, "wb") as delfile:
@@ -38,5 +46,5 @@ for root, subdirs, files in os.walk(PATH):
     	else:
     		output = item[:-4]
     	print output
-    	xor(data, key, output)
+    	fastXor(data, key, output)
     	secure_delete_rand(item)
